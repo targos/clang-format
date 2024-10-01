cecc858874dd05d8552fec01955cc9e75402cc4f
Michaël Zasso <targos@protonmail.com>
2024-10-01 13:09:07 +0200

    add basic build workflow

---
+++ scripts/build-macos.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+mkdir llvm-project/build
+cd llvm-project/build
+cmake -G Ninja -DCMAKE_BUILD_TYPE=MinSizeRel -DLLVM_ENABLE_PROJECTS=clang ../llvm
+ninja clang-format
+strip bin/clang-format
